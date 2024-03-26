f3d4babdb00087e0977eed14325a805c2da4d20b
Gianni Schiralli <gschiralli@hotmail.com>
2024-03-26 16:28:16 -0400

    Add: Homepage Layout Skeleton

---
+++ lingo/app/(marketing)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <p className=" text-green-500 font-bold">This is a marketing page!</p>
+    </div>
+  );
+}
--- lingo/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div>
-      <p className=" text-green-500 font-bold">Hello lingo!</p>
-    </div>
-  );
-}
