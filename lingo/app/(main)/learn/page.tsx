5b8385ff2b093b58e29aa32e91d6605dd4729386
Gianni Schiralli <gschiralli@hotmail.com>
2024-03-30 23:19:25 -0400

    Add: user progress schema

--- lingo/app/(main)/learn/page.tsx
+++ lingo/app/(main)/learn/page.tsx
@@ -2,8 +2,16 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { userProgress } from "@/db/schema";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-const LearnPage = ({}) => {
+const LearnPage = async ({}) => {
+  const [userProgress] = await Promise.all([getUserProgress()]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
